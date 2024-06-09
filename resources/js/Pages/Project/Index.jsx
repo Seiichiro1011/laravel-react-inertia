15646100b3d5ff1f71a6e91ce0ad851c4e117078
Seiichiro1011 <skyblue1011@icloud.com>
2024-06-09 22:10:02 +0900

    add status color and text

--- resources/js/Pages/Project/Index.jsx
+++ resources/js/Pages/Project/Index.jsx
@@ -1,5 +1,7 @@
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
 import Pagination from "@/Components/Pagination";
+import { PROJECT_STATUS_TEXT_MAP } from "@/constants";
+import { PROJECT_STATUS_CLASS_MAP } from "@/constants";
 import { Head, Link } from "@inertiajs/react";
 
 export default function Index({ auth, projects }) {
@@ -30,13 +32,17 @@ export default function Index({ auth, projects }) {
                 </thead>
                 <tbody>
                   {projects.data.map((project) => (
-                  <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                  <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700" key={project.id}>
                     <td className="px-3 py-2">{project.id}</td>
                     <td className="px-3 py-2">
                       <img src={project.image_path} style={{width:60}}/>
                     </td>
                     <td className="px-3 py-2">{project.name}</td>
-                    <td className="px-3 py-2">{project.status}</td>
+                    <td className="px-3 py-2">
+                      <span className={"px-2 py-1 rounded text-white " + PROJECT_STATUS_CLASS_MAP[project.status]}>
+                      {PROJECT_STATUS_TEXT_MAP[project.status]}
+                      </span>
+                      </td>
                     <td className="px-3 py-2">{project.created_at}</td>
                     <td className="px-3 py-2 text-nowrap">{project.due_date}</td>
                     <td className="px-3 py-2 text-nowrap">{project.createdBy.name}</td>
