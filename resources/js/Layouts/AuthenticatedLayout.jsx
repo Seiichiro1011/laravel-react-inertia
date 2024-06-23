c3770594cfb4eb408658df02eb555c0fa7c351e6
Seiichiro1011 <skyblue1011@icloud.com>
2024-06-23 20:12:22 +0900

    changed link

--- resources/js/Layouts/AuthenticatedLayout.jsx
+++ resources/js/Layouts/AuthenticatedLayout.jsx
@@ -21,18 +21,30 @@ export default function Authenticated({ user, header, children }) {
                             </div>
 
                             <div className="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                                <NavLink href={route('dashboard')} active={route().current('dashboard')}>
-                                    Dashboard
-                                </NavLink>
-                                <NavLink href={route('project.index')} active={route().current('project')}>
-                                    Project
-                                </NavLink>
-                                <NavLink href={route('task.index')} active={route().current('task')}>
-                                    All Tasks
-                                </NavLink>
-                                <NavLink href={route('user.index')} active={route().current('user')}>
-                                    User
-                                </NavLink>
+                            <NavLink
+                  href={route("dashboard")}
+                  active={route().current("dashboard")}
+                >
+                  Dashboard
+                </NavLink>
+                <NavLink
+                  href={route("project.index")}
+                  active={route().current("project.index")}
+                >
+                  Projects
+                </NavLink>
+                <NavLink
+                  href={route("task.index")}
+                  active={route().current("task.index")}
+                >
+                  All Tasks
+                </NavLink>
+                <NavLink
+                  href={route("user.index")}
+                  active={route().current("user.index")}
+                >
+                  Users
+                </NavLink>
                             </div>
                         </div>
 
