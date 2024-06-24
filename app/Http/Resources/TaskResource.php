4f839ef58222fdbbc6de6155eafd6b3fdd6340e3
Seiichiro1011 <skyblue1011@icloud.com>
2024-06-24 22:19:11 +0900

    add task

--- app/Http/Resources/TaskResource.php
+++ app/Http/Resources/TaskResource.php
@@ -27,9 +27,7 @@ class TaskResource extends JsonResource
             'due_date' => (new Carbon($this->due_date))->format('Y-m-d'),
             'status' => $this->status,
             'priority' => $this->priority,
-            'image_path' => $this->image_path && !(str_starts_with($this->image_path, 'http')) ?
-                Storage::url($this->image_path) : '',
-            'project_id' => $this->project_id,
+            'image_path' => $this->image_path,
             'project' => new ProjectResource($this->project),
             'assigned_user_id' => $this->assigned_user_id,
             'assignedUser' => $this->assignedUser ? new UserResource($this->assignedUser) : null,
