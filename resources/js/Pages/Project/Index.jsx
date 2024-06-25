0c61aca33c64888f2bcb62adb0255f219ca0305d
Seiichiro1011 <skyblue1011@icloud.com>
2024-06-25 23:12:13 +0900

    add project detail

--- resources/js/Pages/Project/Index.jsx
+++ resources/js/Pages/Project/Index.jsx
@@ -139,7 +139,11 @@ export default function Index({ auth, projects, queryParams= null }) {
                     <td className="px-3 py-2">
                       <img src={project.image_path} style={{width:60}}/>
                     </td>
-                    <td className="px-3 py-2">{project.name}</td>
+                    <th className="px-3 py-2 text-white text-nowrap hover:underline">
+                      <Link href={route("project.show",project.id)}>
+                    {project.name}
+                    </Link>
+                    </th>
                     <td className="px-3 py-2">
                       <span className={"px-2 py-1 rounded text-white " + PROJECT_STATUS_CLASS_MAP[project.status]}>
                       {PROJECT_STATUS_TEXT_MAP[project.status]}
